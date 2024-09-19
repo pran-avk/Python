e6724c8a4ae8698b58d0b6cd4486797e7e0c513b
pran-avk <kp755505@gmail.com>
2024-09-19 18:57:52 +0530

    initial dsa solving commit1

---
+++ DSA solving1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def inSequence(self,s:str,t:str)->bool:
+        i,j=0,0
+        s_len,t_len=len(s),len(t)
+        
+
+        while i<s_len and j<t_len:
+            if s[i]==t[j]:
+                i+=1
+            j+=1
+        
+        return i==s_len
+    
+
+sol = Solution()
+print(sol.inSequence("abc", "ahbgdc"))  
+print(sol.inSequence("axc", "ahbgdc"))  
